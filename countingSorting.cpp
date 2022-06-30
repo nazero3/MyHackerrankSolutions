727f2ae6c2ab2990ab1e101539b3d67c802dec46
nazero3 <nazerrabah1994@gmail.com>
2022-06-30 16:13:52 +0300

    count sorting

--- countingSorting.cpp
+++ countingSorting.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
+vector<int> countingSort(vector<int> a){
+    vector<int> answer;
+    for (int i = 0; i < 100; i++)
+    {
+        answer.push_back(0);
+    }
+    for (int i = 0; i < a.size(); i++)
+    {
+        answer.at(a.at(i)) += 1;
+    }
     
+    return answer;
+}
+int main(){
 
-
+    vector<int> x ={1,1,1,3,4};
+    countingSort(x);
     return 0;
 }
